40be28f93262b75651ad0acc0508b0512d340a29
thansafathima <thansafathimaa@gmail.com>
2026-08-17 21:53:46 +0530

    Add files via upload

---
+++ items.py
@@ -0,0 +1,67 @@
+weapons = [
+    {"name": "Wooden Sword", "rank": "Common", "damage": 5, "crit": 0, "price": 50, "level": 1},
+    {"name": "Rusty Axe", "rank": "Common", "damage": 6, "crit": 0, "price": 60, "level": 1},
+    {"name": "Training Bow", "rank": "Common", "damage": 5, "crit": 1, "price": 55, "level": 1},
+
+    {"name": "Iron Sword", "rank": "Rare", "damage": 12, "crit": 5, "price": 150, "level": 5},
+    {"name": "Steel Axe", "rank": "Rare", "damage": 14, "crit": 5, "price": 170, "level": 5},
+    {"name": "Long Bow", "rank": "Rare", "damage": 11, "crit": 7, "price": 160, "level": 5},
+
+    {"name": "Crystal Blade", "rank": "Super Rare", "damage": 20, "crit": 8, "price": 300, "level": 10},
+    {"name": "Shadow Dagger", "rank": "Super Rare", "damage": 18, "crit": 10, "price": 320, "level": 10},
+    {"name": "Lightning Bow", "rank": "Super Rare", "damage": 19, "crit": 9, "price": 330, "level": 10},
+
+    {"name": "Flame Sword", "rank": "Epic", "damage": 28, "crit": 12, "price": 500, "level": 20},
+    {"name": "Ice Spear", "rank": "Epic", "damage": 27, "crit": 13, "price": 520, "level": 20},
+    {"name": "Thunder Hammer", "rank": "Epic", "damage": 30, "crit": 10, "price": 550, "level": 20},
+
+    {"name": "Phoenix Blade", "rank": "Mythical", "damage": 40, "crit": 18, "price": 800, "level": 30},
+    {"name": "Demon Slayer", "rank": "Mythical", "damage": 42, "crit": 20, "price": 850, "level": 30},
+    {"name": "Celestial Staff", "rank": "Mythical", "damage": 38, "crit": 19, "price": 820, "level": 30},
+    {"name": "Dragon King's Sword", "rank": "Legendary", "damage": 55, "crit": 25, "price": 1200, "level": 50},
+    {"name": "Blade of Eternity", "rank": "Legendary", "damage": 58, "crit": 27, "price": 1300, "level": 60},
+    {"name": "Bow of the Gods", "rank": "Legendary", "damage": 56, "crit": 28, "price": 1350, "level": 60},
+    {"name": "Demon King's Blade", "rank": "Legendary", "damage": 65, "crit": 30, "price": 1600, "level": 80},
+    {"name": "Eternal Destroyer", "rank": "Legendary", "damage": 70, "crit": 35, "price": 2000, "level": 90}
+]
+
+armors = [
+    {"name": "Cloth Armor", "rank": "Common", "defense": 5, "hp": 20, "mana": 10, "crit_res": 1, "price": 50, "level": 1},
+    {"name": "Leather Armor", "rank": "Common", "defense": 7, "hp": 25, "mana": 10, "crit_res": 2, "price": 70, "level": 1},
+
+    {"name": "Iron Armor", "rank": "Rare", "defense": 12, "hp": 40, "mana": 15, "crit_res": 4, "price": 150, "level": 5},
+    {"name": "Steel Armor", "rank": "Rare", "defense": 15, "hp": 50, "mana": 15, "crit_res": 5, "price": 180, "level": 10},
+
+    {"name": "Crystal Armor", "rank": "Super Rare", "defense": 20, "hp": 70, "mana": 30, "crit_res": 7, "price": 300, "level": 15},
+    {"name": "Shadow Armor", "rank": "Super Rare", "defense": 22, "hp": 65, "mana": 35, "crit_res": 8, "price": 350, "level": 20},
+
+    {"name": "Flame Armor", "rank": "Epic", "defense": 28, "hp": 90, "mana": 40, "crit_res": 10, "price": 500, "level": 25},
+    {"name": "Ice Armor", "rank": "Epic", "defense": 30, "hp": 100, "mana": 45, "crit_res": 11, "price": 550, "level": 30},
+
+    {"name": "Phoenix Armor", "rank": "Mythical", "defense": 38, "hp": 130, "mana": 60, "crit_res": 14, "price": 800, "level": 40},
+    {"name": "Demon Armor", "rank": "Mythical", "defense": 42, "hp": 150, "mana": 70, "crit_res": 16, "price": 900, "level": 50},
+
+    {"name": "Celestial Armor", "rank": "Legendary", "defense": 50, "hp": 180, "mana": 90, "crit_res": 20, "price": 1200, "level": 70},
+    {"name": "Dragon Armor", "rank": "Legendary", "defense": 60, "hp": 220, "mana": 100, "crit_res": 25, "price": 1500, "level": 90}
+]
+
+potions = [
+    {"name": "Small Health Potion", "type": "health", "value": 50, "price": 30},
+    {"name": "Medium Health Potion", "type": "health", "value": 100, "price": 50},
+    {"name": "Large Health Potion", "type": "health", "value": 250, "price": 100},
+    {"name": "Giant Health Potion", "type": "health", "value": 500, "price": 180},
+    {"name": "Ultimate Health Potion", "type": "health", "value": 9999, "price": 300},
+
+    {"name": "Small Mana Potion", "type": "mana", "value": 30, "price": 30},
+    {"name": "Medium Mana Potion", "type": "mana", "value": 75, "price": 50},
+    {"name": "Large Mana Potion", "type": "mana", "value": 150, "price": 100},
+    {"name": "Giant Mana Potion", "type": "mana", "value": 300, "price": 180},
+    {"name": "Ultimate Mana Potion", "type": "mana", "value": 9999, "price": 300},
+
+    {"name": "Health Mana Potion", "type": "mixed", "value": 100, "price": 150},
+    {"name": "Greater Mixed Potion", "type": "mixed", "value": 250, "price": 250},
+
+    {"name": "Attack Potion", "type": "attack", "value": 10, "price": 120},
+    {"name": "Defense Potion", "type": "defense", "value": 10, "price": 120},
+    {"name": "Critical Potion", "type": "crit", "value": 10, "price": 150}
+]
